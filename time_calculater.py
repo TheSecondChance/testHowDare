2da1a883e30dc9a3e417e6cf271c61418ed95c23
TheSecondChance <mesakmegerem@gmail.com>
2025-11-20 16:50:23 +0300

    Refactor time tracking classes
    - Added an `elapsed` method to the `TimeCalculator` class to return the elapsed time since instantiation.
    - Renamed `elapsed` method to `check_elapsed` in the `TimeChecker` class for clarity and updated its docstring.
    - Removed unused `reset` and `Tracker` class methods from `TimeChecker` to streamline the code.

--- time_calculater.py
+++ time_calculater.py
@@ -3,3 +3,5 @@ from time import time
 class TimeCalculator:
     def __init__(self):
         self.start = time()
+    def elapsed(self):
+        return time() - self.start
--- time_checker.py
+++ time_checker.py
@@ -3,28 +3,13 @@ from time import time
 class TimeChecker:
     def __init__(self):
         self.start_time = None
-
+    
     def start(self):
         """Start the timer."""
         self.start_time = time()
 
-    def elapsed(self):
-        """Return the elapsed time since start was called."""
+    def check_elapsed(self):
+        """Check the elapsed time since the timer was started."""
         if self.start_time is None:
-            raise ValueError("Timer has not been started. Call start() before elapsed().")
+            raise ValueError("Timer has not been started.")
         return time() - self.start_time
-
-    def reset(self):
-        """Reset the timer."""
-        self.start_time = time()
-
-class Tracker:
-    def __init__(self):
-        self.events = []
-
-    def log_event(self, event_name):
-        """Log an event with the current timestamp."""
-        self.events.append((event_name, time()))
-
-    def get_events(self):
-        return self.events
