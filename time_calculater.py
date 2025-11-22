326b5952d28a56495da46e138ac4a3605d2e6c68
TheSecondChance <mesakmegerem@gmail.com>
2025-11-22 19:58:29 +0300

    Add stats.json to track file modifications and user interactions

--- time_calculater.py
+++ time_calculater.py
@@ -5,3 +5,10 @@ class TimeCalculator:
         self.start_time = None
         self.end_time = None
 
+    def start(self):
+        """Start the timer."""
+        self.start_time = time()
+    
+    def stop(self):
+        """Stop the timer."""
+        self.end_time = time()
